c9f473ea42ca4cb8927a24749b7b8ce95d5a5b55
Rayched <ny2401@naver.com>
2022-11-30 15:31:49 +0900

    ãSetting

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== 게시판 v 0.1 ===");
+        System.out.println("=== 프로그램 시작 ===");
+        System.out.printf("명령) \n");
+        System.out.println("=== 프로그램 종료 ===");
+    }
+}

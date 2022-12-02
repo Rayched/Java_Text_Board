b1fd66ed2fa940572405d5f44cae3753724aa89b
Rayched <ny2401@naver.com>
2022-12-02 15:47:56 +0900

    v 0.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== 게시판 v 0.1 ===");
         System.out.println("=== 프로그램 시작 ===");
         System.out.printf("명령) \n");
+        Scanner cmd = new Scanner(System.in);
+        String text = cmd.nextLine();
+        System.out.printf("입력받은 명령어 : %s\n", text);
         System.out.println("=== 프로그램 종료 ===");
     }
 }
